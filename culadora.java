c60824af701a1cd2a1754ecb5ba2c926b538b6bb
dbeNit0 <davidingles07@gmail.com>
2025-11-20 09:50:49 +0100

    Nuevo comentario en el archivo calculadora

--- culadora.java
+++ culadora.java
@@ -5,6 +5,7 @@ public class Mavenproject1 {
 
 //Realizamos cambios en el archivo calculadora desde Github
 
+//Realizamos cambios desde el equipo local
 
     public static void main(String[] args) {
         System.out.println("Este es el archivo principal del repositorio calculadora");
